7bcd05f9cd5548259a32cef08849fe9c481eda7f
Ahad-UCF <Ahadbawany@knights.ucf.edu>
2019-06-19 10:54:08 -0400

    Compartmentalized code and created a way to cleanly print stories

--- Story.go
+++ Story.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"encoding/json"
+	"flag"
 )
 
 
@@ -27,25 +28,45 @@ type Option struct {
 }
 
 func main(){
-
 	// Generate a map that we will later dump the json into
 	var story Story
+	decodeJson(grabJson(), &story)
+	printArc("intro", &story)
+}
 
+func grabJson() (*os.File){
+	fileName := flag.String("json_File", "gopher.json", "The json file's name")
 	// Grab the json file and make sure it exists
-	jsonFile, err := os.Open("gopher.json")
+	jsonFile, err := os.Open(*fileName)
 	checkErr(err)
+
+	return jsonFile
+}
+
+func decodeJson(jsonFile *os.File, storyptr *Story){
 
 	// Decode the json file and dump it into the map
 	decoder := json.NewDecoder(jsonFile)
-	err = decoder.Decode(&story)
+	err := decoder.Decode(storyptr)
 
 	// Verify the decode resolved
 	checkErr(err)
 
-	// TODO: Remove this line, for testing purposes only
-	fmt.Printf("%+v\n",story)
+	/* TODO: Remove this line, for testing purposes only
+	fmt.Println((*storyptr)["intro"].Title)
+	fmt.Println((*storyptr)["intro"].Story)
+	fmt.Println((*storyptr)["intro"].Options[0].Text)
+	fmt.Println((*storyptr)["intro"].Options[1].Text)*/
 }
 
+
+// Print the title of each story_arc followed by the actual story for that chapter
+func printArc(Title string,storyptr *Story){
+	fmt.Println((*storyptr)[Title].Title)
+	for i := 0; i < len((*storyptr)[Title].Story); i++{
+		fmt.Println((*storyptr)[Title].Story[i])
+	}
+}
 
 // Verify that an error did not occur, if it did... panic!
 func checkErr(err error){
